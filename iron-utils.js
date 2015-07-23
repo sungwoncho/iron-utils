e8b32a4b370228d3ff7d4886634f5ced9eeb7481
Sung Won Cho <mikeswcho@gmail.com>
2015-07-24 09:58:15 +1000

    Add .is, .params, .getName utilities

--- iron-utils.js
+++ iron-utils.js
@@ -3,3 +3,17 @@
  * @namespace
  */
 CurrentRoute = {};
+
+CurrentRoute.getName = function () {
+  var currentPage = Router.current();
+  return currentPage.route.getName();
+};
+
+CurrentRoute.is = function (routeName) {
+  var currentPage = Router.current();
+  return currentPage.route.getName() === routeName;
+};
+
+CurrentRoute.params = function () {
+  return currentPage.params;
+};
